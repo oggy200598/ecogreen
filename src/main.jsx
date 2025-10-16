7e1416afa3e8b344c4de411d7acafa53b22e2657
Huy Nguyen <oggy200598@gmail.com>
2025-10-16 13:29:14 +0700

    update commitAnalytic

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { SpeedInsights } from '@vercel/speed-insights/react' // ✅ import bản React
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react" // ✅ đúng
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
