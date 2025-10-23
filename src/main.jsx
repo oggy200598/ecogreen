9ec18db230ae13c1c1fbcdf8850a722c82292400
Huy Nguyen <oggy200598@gmail.com>
2025-10-23 14:44:44 +0700

    remove react-helmet-async conflict

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App'
 import './index.css'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import { Analytics } from '@vercel/analytics/react'
-import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
