45968a342d5251025f96f780f471378ff3d1765b
Huy Nguyen <oggy200598@gmail.com>
2025-11-09 14:42:36 +0700

    Fix build for sitemap

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,3 @@
-// vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -8,6 +7,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    mimeTypes: {
+      'xml': 'application/xml',
     },
   },
 })
